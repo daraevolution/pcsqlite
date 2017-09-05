787108cd256c27785afb2c85e0f7bd8a53223434
Daraevolution <daraevolution@gmail.com>
2017-09-05 23:36:57 +0200

    create db name from std::cin

--- openclosesqlite.cpp
+++ openclosesqlite.cpp
@@ -5,24 +5,31 @@
 
 int main(int argc, char* argv[])
 {
-   sqlite3 *db;
-   int res;
+    sqlite3 *db;
+    int res;
+    std::string dbname;
 
-   /* Open database */
-   res = sqlite3_open("test.db", &db);
-   if (res)
-   {
-      fprintf(stderr, "No puedo abrir la base de datos: %s\n", sqlite3_errmsg(db));
-      std::cout << "No puedo abrir la base de datos:" << sqlite3_errmsg(db) << '\n';
-      exit(0);
-   }
-   else
-   {
-      fprintf(stderr, "Base de datos OK\n");
-      std::cout << "Base de datos OK" << '\n';
-   }
+    std::cout << "Introduce el nombre de la db" << "\n";
+    std::cin >> dbname;
+    dbname.append(".db");
 
-   sqlite3_close(db);
+    std::cout << "Se creara la siguiente base de datos: " << dbname << "\n";
+    const char *db_name = dbname.c_str();
+    /* Open database */
+    res = sqlite3_open(db_name , &db);
+    if (res)
+    {
+        fprintf(stderr, "No puedo abrir la base de datos: %s\n", sqlite3_errmsg(db));
+        std::cout << "No puedo abrir la base de datos:" << sqlite3_errmsg(db) << '\n';
+        exit(0);
+    }
+    else
+    {
+        fprintf(stderr, "Base de datos OK\n");
+        std::cout << "Base de datos OK" << '\n';
+    }
 
-   return 0;
+    sqlite3_close(db);
+
+    return 0;
 }
